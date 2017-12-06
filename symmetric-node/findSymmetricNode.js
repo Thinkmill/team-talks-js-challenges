9a3ce05c8efbf092cadf06da43f117caa0ff0ea7
Cameron Bourke <cameron2295@gmail.com>
2017-12-06 15:31:44 +1100

    Add instruction to open .html file

--- symmetric-node/findSymmetricNode.js
+++ symmetric-node/findSymmetricNode.js
@@ -1,5 +1,6 @@
 /*
  * Given two identical DOM tree structures, A and B, and a node from A, find the corresponding node in B.
+ * Take a look at ./symmetric_node.html in the browser
  */
 
 /* @returns {Element} - corresponding dom element in root2 */
